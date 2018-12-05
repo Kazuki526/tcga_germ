3678d2d63a56b85b41f75856faff1a7f9b2ad679
Kazuki:ilg1 <takaha5.26@gmail.com>
2018-12-05 20:58:20 +0900

    download

--- variant_call/download.pl
+++ variant_call/download.pl
@@ -8,9 +8,20 @@ if(!defined $ARGV[0]){die "ERROR::plese imput P_ID:patient_id";}
 my $pid = uc $ARGV[0];
 my $bam_json = "$pid.json";
 
+# check token file
+my $token_path=`ls ~/git/innanlab/gdc|grep 'gdc-user-token'`;
+if(!$token_path){die "!!ERROR!!:token file not exitst!!";}
+chomp $token_path;
+$token_path="~/git/innanlab/gdc/$token_path";
+my $token=`cat $token_path`;
+
 #check nkf was installed??
 my $nkfpath=`which nkf`;chomp $nkfpath;
 ($nkfpath and -e $nkfpath) or die "ERROR:nkf was not installed. please do\nbrew install nkf\n";
+
+#check gdc-clinent exist??
+my $gdc_client = "~/gdc-client";
+(-e $gdc_client) or die "ERROR::gdc-client does not exits!\n";
 
 #check existing sample_bam.json
 my $sample_json="$ENV{HOME}/git/tcga/variant_call/sample_bam.json";
@@ -24,11 +35,10 @@ my $response = "bam_info.tsv"
 
 
 # rad response and make norm & tumor manifest
+my (@norm_file,@tumor_file)=((),());
+my (@norm_id,@tumor_id)=((),());
 my ($norm_manifest, $tumor_manifest) = ("norm_bam/norm_manifest.tsv","tumor_bam/tumor_manifest.tsv");
 open(RES,"$response");
-open(OUTN,">$norm_manifest");
-open(OUTT,">$tumor_manifest");
-print OUTN "id\tfilename\n";print OUTT "id\tfilename\n";
 my @header = split(/\t/,<RES>);chomp @header;
 my %header_num=();
 for(my $i=0;@header>$i;$i++){
@@ -42,91 +52,54 @@ while(<RES>){
 		chomp;
 		my @line=split(/\t/,);
 		if($line[$header_num{sample_type}] eq "Primary Tumor"){
-				print OUTT "$line[$header_num{file_id}]\t$line[$header_num{file}]\n";
+				`$gdc_client download -t $token_path $line[$header_num{file_id}]`;
+				my $partial =&check_download($line[$header_num{file_id}],$line[$header_num{file}]);
+				while($partial == "true"){
+						`$gdc_client download -t $token_path $line[$header_num{file_id}]`;
+						$partial =&check_download($line[$header_num{file_id}],$line[$header_num{file}]);
+				}
+				`rm -rf $line[$header_num{file_id}]/logs`;
+				`mv $line[$header_num{file_id}]/* tumor_bam/`;
+				push(@tumor_file,$line[$header_num{file}]);
+				push(@tumor_id,$line[$header_num{file_id}]);
 		}else{
-				print OUTN "$line[$header_num{file_id}]\t$line[$header_num{file}]\n";
+				`$gdc_client download -t $token_path $line[$header_num{file_id}]`;
+				my $partial =&check_download($line[$header_num{file_id}],$line[$header_num{file}]);
+				while($partial == "true"){
+						`$gdc_client download -t $token_path $line[$header_num{file_id}]`;
+						$partial =&check_download($line[$header_num{file_id}],$line[$header_num{file}]);
+				}
+				`rm -rf $line[$header_num{file_id}]/logs`;
+				`mv $line[$header_num{file_id}]/* norm_bam/`;
+				push(@norm_file,$line[$header_num{file}]);
+				push(@norm_id,$line[$header_num{file_id}]);
 		}
 }
 close RES;
 close OUTN;
 close OUTT;
 
-
-system("perl $download_pl $norm_manifest $bed $project_dir/norm_bam");
-system("perl $download_pl $tumor_manifest $bed $project_dir/tumor_bam");
-
-
-
-#!/usr/bin/perl
-use strict;
-use warnings;
-use Parallel::ForkManager;
-
-############   perl bamslicing.pl  MANIFEST BED_JSON OUT_DIR
-
-#input check?
-my ($manifest,$json,$out_dir)=@ARGV;
-if( $manifest !~/manifest/|$json !~/json$/){die "ERROR::input files are correct??\n";}
-(-e $out_dir) or die "ERROR::out dir is not exist!\n";
-
-#perl bamslicing.pl MANIFEST.tsv out_DIR maxparallel
-my $token_path=`ls ~/git/innanlab/gdc|grep 'gdc-user-token'`;
-if(!$token_path){die "!!ERROR!!:token file not exitst!!";}
-chomp $token_path;
-$token_path="~/git/innanlab/gdc/$token_path";
-my $token=`cat $token_path`;
-
-($manifest and -e "$manifest") or die "ERROR: there arenot manifest file!!\n";
-($json     and -e "$json"    ) or die "ERROR: there arenot json file!!\n";
-
-open(MAN,"$manifest");
-my $linen=0;
-my $max_processes=10;
-my $pm = new Parallel::ForkManager($max_processes);
-my $header=<MAN>;chomp $header;
-my @header = split(/\t/,$header);
-if(($header[0] ne "id")||($header[1] ne "filename")){die "ERROR::manifest file colume is correct??\n";}
-
-open (ERR,">$out_dir/result_download.txt");
-while(<MAN>){
-		$linen++;
-		#forks and returns the pid for child
-		my $pid = $pm->start and next;
-
-#		if($start_line_num > $line_num){next;
-#		}elsif($end_line_num < $line_num){last;}
-		chomp;
-		my @line=split(/\t/,);
-		my @ls=`ls $out_dir`;chomp @ls;
-		if(!grep{$_ eq "$line[1]"}@ls){
-				print "$linen:curl $line[1] now\n";
-				print ERR "$linen:curl $line[1] now\n";
-				system("curl --header \"X-Auth-Token: $token\" --request POST https://api.gdc.cancer.gov/slicing/view/$line[0] --header \"Content-Type: application/json\" -d\@$json --output $out_dir/$line[1] > /dev/null 2>&1");
-		}
-		my $focal=0;
-		my $time=0;
-		while($focal==0){
-				$time++;
-				my $sam_head = `samtools view $out_dir/$line[1] 2>&1|head -n 1`;
-				if($time >10){
-						print "$linen:$line[1] download more than 10 times so this file cannot download?\n";
-						print ERR "$linen:$line[1] download more than 10 times so this file cannot download?\n";
-						$focal++;
-				}elsif(($sam_head !~ /EOF\smarker\sis\sabsent/) && ($sam_head !~ /Parse\serror/)){
-						$focal++;
-						print "$linen:$line[1] downloaded is ok\n";
-						print ERR "$linen:$line[1] downloaded is ok\n";
-				}else{
-						print "$linen:$line[1] download error. download again\n";
-						print ERR "$linen:$line[1] download error. download again\n";
-				}
-				if($focal==0){
-						system("curl --header \"X-Auth-Token: $token\" --request POST https://api.gdc.cancer.gov/slicing/view/$line[0] --header \"Content-Type: application/json\" -d\@$json --output $out_dir/$line[1] > /dev/null 2>&1");
-				}
-		}
-
-		$pm->finish; #terminates the child process
-}
-close MAN;
-close ERR;
 exit;
+
+sub check_download( $ $ ){
+		my($file_id,$file)=@_;
+		my $partial="false";
+		my @ls = `ls $file_id`;chomp @ls;
+		# partial check
+		foreach my $f (@ls){
+				if($f =~/partial$/){$partial="true";}
+		}
+		if($partial=="true"){
+				return($partial);
+		}else{
+				# bai check
+				my $bai=0;
+				foreach my $f (@ls){
+						if($f =~/bai$/){$bai++;}
+				}
+				if($bai==0){`samtools index $file_id/$file`;}
+		}
+		return($partial);
+}
+		
+
